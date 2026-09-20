2531cf61eda7e32cb53b2ff1adf409f0e9c56991
RIG360 <134787168+RIG360@users.noreply.github.com>
2026-09-20 18:56:46 +0200

    commit

---
+++ data.js
@@ -0,0 +1,659 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-breit27-kg-stiege_260912",
+      "name": "Breit27-KG-Stiege_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6333425725737385,
+          "pitch": 0.2791054029683391,
+          "rotation": 0,
+          "target": "1-breit27-kg-mitteltrakt_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-breit27-kg-mitteltrakt_260912",
+      "name": "Breit27-KG-Mitteltrakt_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3652410147815104,
+          "pitch": -0.06443693117770621,
+          "rotation": 0,
+          "target": "0-breit27-kg-stiege_260912"
+        },
+        {
+          "yaw": -0.10493211981373562,
+          "pitch": 0.045808681806423834,
+          "rotation": 0,
+          "target": "2-breit27-kg-hoftrakt1_260912"
+        },
+        {
+          "yaw": 2.984331609596609,
+          "pitch": 0.25496261660113184,
+          "rotation": 0,
+          "target": "10-breit27-kg-straentrakt1_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-breit27-kg-hoftrakt1_260912",
+      "name": "Breit27-KG-Hoftrakt1_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.140913486600053,
+          "pitch": 0.21455376763535483,
+          "rotation": 0,
+          "target": "1-breit27-kg-mitteltrakt_260912"
+        },
+        {
+          "yaw": 1.4266563625296191,
+          "pitch": 0.06481174418693314,
+          "rotation": 0,
+          "target": "3-breit27-kg-hoftrakt2_260912"
+        },
+        {
+          "yaw": 0.5961483107855265,
+          "pitch": -0.015594814176923322,
+          "rotation": 0,
+          "target": "4-breit27-kg-hoftrakt3_260912"
+        },
+        {
+          "yaw": -1.4389503153881442,
+          "pitch": 0.03884800324087223,
+          "rotation": 0,
+          "target": "9-breit27-kg-hoftrakt8_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-breit27-kg-hoftrakt2_260912",
+      "name": "Breit27-KG-Hoftrakt2_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6310461733187012,
+          "pitch": 0.12182527333013127,
+          "rotation": 0,
+          "target": "2-breit27-kg-hoftrakt1_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-breit27-kg-hoftrakt3_260912",
+      "name": "Breit27-KG-Hoftrakt3_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.975430110871825,
+          "pitch": 0.26818083460513265,
+          "rotation": 0,
+          "target": "2-breit27-kg-hoftrakt1_260912"
+        },
+        {
+          "yaw": 0.011307795039463997,
+          "pitch": 0.07524136207966592,
+          "rotation": 0,
+          "target": "5-breit27-kg-hoftrakt4_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-breit27-kg-hoftrakt4_260912",
+      "name": "Breit27-KG-Hoftrakt4_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0668223843736246,
+          "pitch": 0.33963420670009015,
+          "rotation": 0,
+          "target": "4-breit27-kg-hoftrakt3_260912"
+        },
+        {
+          "yaw": -1.488768138689533,
+          "pitch": 0.17109494267452874,
+          "rotation": 0,
+          "target": "6-breit27-kg-hoftrakt5_260912"
+        },
+        {
+          "yaw": 1.6637992293638533,
+          "pitch": 0.04213511596904951,
+          "rotation": 0,
+          "target": "8-breit27-kg-hoftrakt7_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-breit27-kg-hoftrakt5_260912",
+      "name": "Breit27-KG-Hoftrakt5_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5447745396059762,
+          "pitch": 0.21732686892905662,
+          "rotation": 0,
+          "target": "5-breit27-kg-hoftrakt4_260912"
+        },
+        {
+          "yaw": -1.4779298394038403,
+          "pitch": 0.09036801237107461,
+          "rotation": 0,
+          "target": "7-breit27-kg-hoftrakt6_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-breit27-kg-hoftrakt6_260912",
+      "name": "Breit27-KG-Hoftrakt6_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5226763792611395,
+          "pitch": 0.08306699922510496,
+          "rotation": 0,
+          "target": "6-breit27-kg-hoftrakt5_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-breit27-kg-hoftrakt7_260912",
+      "name": "Breit27-KG-Hoftrakt7_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5311795854973518,
+          "pitch": 0.12821883281632296,
+          "rotation": 0,
+          "target": "5-breit27-kg-hoftrakt4_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-breit27-kg-hoftrakt8_260912",
+      "name": "Breit27-KG-Hoftrakt8_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5231762156575215,
+          "pitch": -0.005529274728987232,
+          "rotation": 0,
+          "target": "2-breit27-kg-hoftrakt1_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-breit27-kg-straentrakt1_260912",
+      "name": "Breit27-KG-Straßentrakt1_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.007538704342973546,
+          "pitch": 0.2509118885800561,
+          "rotation": 0,
+          "target": "1-breit27-kg-mitteltrakt_260912"
+        },
+        {
+          "yaw": -1.7579632335660964,
+          "pitch": 0.2782758871072346,
+          "rotation": 0,
+          "target": "11-breit27-kg-straentrakt2_260912"
+        },
+        {
+          "yaw": 1.779037251832186,
+          "pitch": 0.06445826683578204,
+          "rotation": 0,
+          "target": "12-breit27-kg-straentrakt3_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-breit27-kg-straentrakt2_260912",
+      "name": "Breit27-KG-Straßentrakt2_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.505246137237684,
+          "pitch": 0.0912290464120602,
+          "rotation": 0,
+          "target": "10-breit27-kg-straentrakt1_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-breit27-kg-straentrakt3_260912",
+      "name": "Breit27-KG-Straßentrakt3_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5018675468540987,
+          "pitch": 0.042947221945983216,
+          "rotation": 0,
+          "target": "10-breit27-kg-straentrakt1_260912"
+        },
+        {
+          "yaw": 3.0629157532018843,
+          "pitch": 0.2533229365691625,
+          "rotation": 0,
+          "target": "13-breit27-kg-straentrakt4_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-breit27-kg-straentrakt4_260912",
+      "name": "Breit27-KG-Straßentrakt4_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06023784896764006,
+          "pitch": 0.17867813559073298,
+          "rotation": 0,
+          "target": "12-breit27-kg-straentrakt3_260912"
+        },
+        {
+          "yaw": -1.6035180268432399,
+          "pitch": 0.12904070373537735,
+          "rotation": 0,
+          "target": "14-breit27-kg-straentrakt5_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-breit27-kg-straentrakt5_260912",
+      "name": "Breit27-KG-Straßentrakt5_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.552163485510012,
+          "pitch": 0.0365846723213572,
+          "rotation": 0,
+          "target": "13-breit27-kg-straentrakt4_260912"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Breit27-KG_260912",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
